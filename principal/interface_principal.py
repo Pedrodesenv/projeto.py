34f838fb962ea496bf876e245cc378b4e6f153ea
Pedro <peuleite15@gmail.com>
2025-05-21 17:33:25 -0300

    partes Pedro

--- principal/interface_principal.py
+++ principal/interface_principal.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
 # CORES PRINCIPAIS- (Se quiser mudar fica a vontade)
-preto = '#000000'
-branco = "#F5F5F5"
+preto = 'black'
+branco = "white"
 verde = "#32CD32"
 
 # CORES DE IMC - tipo uma tabela com a classificação
@@ -13,14 +13,13 @@ verde = peso normal
 
 '''
 
-
-
 #Criando a Janela
 
 janela = tk.Tk()
 janela.title('Calculadora de IMC')
 janela.geometry('350x200')
 janela.config(bg=preto)
+
 
 #Divisão das janelas - superior
 
@@ -37,7 +36,22 @@ font=('Sans Serif',16,'bold'))
 
 label_janela.pack()
 
-#Divisão das janelas - inferior
+# Linha decorativa
+app_linha = tk.Label(
+    janela_superior,
+    text="",
+    width=350,
+    height=1,
+    anchor="center",
+    padx=0,
+    pady=0,
+    relief="flat",
+    font=("ivy 1"),
+    bg=verde,
+    fg=verde
+)
+app_linha.pack()
+
 
 janela_inferior = tk.Frame(janela,
 width=300,
@@ -47,12 +61,29 @@ bg=preto)
 
 janela_inferior.grid(row=1,column=0,sticky='NSEW')
 
+text_peso = tk.Label(janela_inferior, text="insira seu peso", height=1, padx=0, anchor='center', relief="flat", font=("ivy 10 bold "), bg=preto, fg=verde)
+text_peso.grid(row=0, column=0, sticky='NSEW', pady=10, padx=3)
+
+caixa_peso = tk.Entry(janela_inferior, width=5, relief="solid", font=("ivy 10 bold "), justify='center')
+caixa_peso.grid(row=0, column=1, sticky='NSEW', pady=10, padx=3)
+
+text_altura = tk.Label(janela_inferior, text="insira seu peso", height=1, padx=0, anchor='center', relief="flat", font=("ivy 10 bold "), bg=preto, fg=verde)
+text_altura.grid(row=1, column=0, sticky='NSEW', pady=10, padx=3)
+
+caixa_altura = tk.Entry(janela_inferior, width=5, relief="solid", font=("ivy 10 bold "), justify='center')
+caixa_altura.grid(row=1, column=1, sticky='NSEW', pady=10, padx=3)
+
+caixa_resultado = tk.Label(janela_inferior, width=5, height=1, padx=6, pady=12, relief="flat", anchor="center", font=("ivy 25 bold "), bg=verde, fg=verde)
+caixa_resultado.place(x=170, y=10)
+
+l_resultado_texto = tk.Label(janela_inferior, text="seu resultado é: obesidade", width=37, height=1, padx=10, pady=1, relief="flat", anchor="center", font=("ivy 8 bold "), bg=verde, fg=verde)
+l_resultado_texto.place(x=0, y=85)
+
 #Criação da função
 
 def calculo():
     peso = float() # Depois tu usa o entrada_peso.get() que serve como um input
     altura = float()
-
     imc = peso / altura ** 2
     imc_formatado = f'{imc:.2f}' #apenas duas casas decimais
     
