937a061a2bf62ca350b8a33e65fa76d3f77831cd
Pedro <peuleite15@gmail.com>
2025-05-26 11:42:21 -0300

    fechando ajustes

--- principal/interface_principal.py
+++ principal/interface_principal.py
@@ -17,7 +17,7 @@ verde = peso normal
 
 janela = tk.Tk()
 janela.title('Calculadora de IMC')
-janela.geometry('350x200')
+janela.geometry('295x230')
 janela.config(bg=preto)
 
 
@@ -61,45 +61,58 @@ bg=preto)
 
 janela_inferior.grid(row=1,column=0,sticky='NSEW')
 
-text_peso = tk.Label(janela_inferior, text="insira seu peso", height=1, padx=0, anchor='center', relief="flat", font=("ivy 10 bold "), bg=preto, fg=verde)
+text_peso = tk.Label(janela_inferior, text="insira seu peso:", height=1, padx=0, anchor='center', relief="flat", font=("ivy 10 bold "), bg=preto, fg=verde)
 text_peso.grid(row=0, column=0, sticky='NSEW', pady=10, padx=3)
 
 caixa_peso = tk.Entry(janela_inferior, width=5, relief="solid", font=("ivy 10 bold "), justify='center')
 caixa_peso.grid(row=0, column=1, sticky='NSEW', pady=10, padx=3)
 
-text_altura = tk.Label(janela_inferior, text="insira seu peso", height=1, padx=0, anchor='center', relief="flat", font=("ivy 10 bold "), bg=preto, fg=verde)
+text_altura = tk.Label(janela_inferior, text="insira sua altura:", height=1, padx=0, anchor='center', relief="flat", font=("ivy 10 bold "), bg=preto, fg=verde)
 text_altura.grid(row=1, column=0, sticky='NSEW', pady=10, padx=3)
 
 caixa_altura = tk.Entry(janela_inferior, width=5, relief="solid", font=("ivy 10 bold "), justify='center')
 caixa_altura.grid(row=1, column=1, sticky='NSEW', pady=10, padx=3)
 
-caixa_resultado = tk.Label(janela_inferior, width=5, height=1, padx=6, pady=12, relief="flat", anchor="center", font=("ivy 25 bold "), bg=verde, fg=verde)
-caixa_resultado.place(x=170, y=10)
+caixa_resultado = tk.Label(janela_inferior, width=5, height=1, padx=6, pady=12, relief="flat", anchor="center", font=("ivy 25 bold "), bg=verde, fg=branco)
+caixa_resultado.place(x=175, y=10)
 
-l_resultado_texto = tk.Label(janela_inferior, text="seu resultado é: obesidade", width=37, height=1, padx=10, pady=1, relief="flat", anchor="center", font=("ivy 8 bold "), bg=verde, fg=verde)
+l_resultado_texto = tk.Label(janela_inferior, text="", width=37, height=1, padx=10, pady=10, relief="flat", anchor="center", font=("ivy 8 bold "), bg=preto, fg=verde)
 l_resultado_texto.place(x=0, y=85)
+
+
+b_calcular = tk.Button(janela_inferior, command=lambda: calculo(), text="calcular", width=37, height=1, overrelief='solid', relief="raised", anchor='center', font=('ivy 10 bold '), bg=verde, fg=preto)
+b_calcular.grid(row=4, column=0, sticky='NSEW', pady=60, padx=3, columnspan=40)
 
 #Criação da função
 
+
 def calculo():
-    peso = float() # Depois tu usa o entrada_peso.get() que serve como um input
-    altura = float()
+    peso = float(caixa_peso.get())# Depois tu usa o entrada_peso.get() que serve como um input
+    altura = float(caixa_altura.get())
     imc = peso / altura ** 2
     imc_formatado = f'{imc:.2f}' #apenas duas casas decimais
+
+    # Exibir o valor numérico
+    caixa_resultado['text'] = imc_formatado
     
     if imc < 18.5:
-        print('Abaixo do peso')
-    elif imc > 18.5 and imc < 24.9:
-        print('Peso normal')
-    elif imc > 25 and imc < 29.9:
-        print('Sobrepeso')
-    elif imc > 30 and imc < 34.9:
-        print('Obesidade grau I')
-    elif imc > 35 and imc < 39.9:
-        print('Obesidade grau II')
-    else:
-        print('Obesidade grau III')
+         l_resultado_texto['text'] = 'seu imc é: Abaixo do Peso'
 
+    elif imc > 18.5 and imc < 24.9:
+         l_resultado_texto['text'] = 'seu imc é: Peso normal'
+
+    elif imc > 25 and imc < 29.9:
+        l_resultado_texto['text'] = 'seu imc é: sobrepeso'
+
+    elif imc > 30 and imc < 34.9:
+        l_resultado_texto['text'] = 'seu imc é: obesidade grau I'
+
+    elif imc > 35 and imc < 39.9:
+         l_resultado_texto['text'] = 'seu imc é: obesidade grau II'
+
+    else:
+        l_resultado_texto['text'] = 'seu imc é: obesidade grau III'
+    
 
 #Deixando a janela sempre aberta
 janela.mainloop()
